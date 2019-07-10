91ce6ec6318d7ec8319743595778cddb8ae71f79
crt <1747369444@qq.com>
2019-07-10 20:11:59 +0800

    登录成功存储用户token到容器中

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// 引入自己封装的方法
+import * as auth from '@/utils/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    // 初始化的时候从本地储存获取数据，防止刷新丢失 token
+    user: auth.getUser()
   },
   mutations: {
+    // 登录成功，调用mutation更新容器中的 user 的状态
+    setUser (state, data) {
+      state.user = data
 
+      // 将数据放到本地存储
+      auth.saveUser(state.user)
+    }
   },
   actions: {
 

421cb153d16e4f0673d87e490cb546a40179456d
crt <1747369444@qq.com>
2019-07-11 22:00:05 +0800

    首页-布局

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,15 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-    name: 'home',
     path: '/',
-    component: () => import('@/views/home')
+    component: () => import('@/views/tabbar-layout'),
+    children: [{
+      name: 'home',
+      path: '',
+      component: () => import('@/views/home')
+    }]
   },
+
   {
     name: 'login',
     path: '/login',
---
+++ src/views/tabbar-layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 路由出口 -->
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="search" to="/qa">问答</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to="/my">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabbarLayout'
+}
+</script>
+
+<style scoped>
+</style>
